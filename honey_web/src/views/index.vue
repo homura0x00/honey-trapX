d5e073b3c39af0ee18ccd6c91eeb5b2ab30ba842
homura <61085099+rabitIII@users.noreply.github.com>
2025-09-18 19:53:55 +0800

    添加arco.design组件库

---
+++ honey_web/src/views/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Honey home</div>
+</template>
---
+++ honey_web/src/views/login/LoginView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="login-view">
+        Login Page
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
